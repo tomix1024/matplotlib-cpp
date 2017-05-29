4958a1622bc8484bc77d39a987e82772aea2fad0
Tom Kneiphof <tomkneiphof@gmail.com>
2017-05-29 16:26:10 +0200

    Update patches example

--- examples/patches.cpp
+++ examples/patches.cpp
@@ -4,26 +4,34 @@
 
 namespace plt = matplotlibcpp;
 
-int main() 
+int main()
 {
 	auto patch = plt::patches::Ellipse(10, 10, 4, 2, 45);
-    auto patch2 = plt::patches::Rectangle(5, 2, 3, 3, {{"facecolor", "red"}});
+	auto patch2 = plt::patches::Rectangle(5, 2, 3, 3, {{"facecolor", "red"}});
 
-    patch2.set_alpha(.5);
+	patch2.set_alpha(.5);
 
-    patch.set_label("Ellipse");
-    patch2.set_label("Rectangle");
+	patch.set_label("Ellipse");
+	patch2.set_label("Rectangle");
 
-    plt::add_patch(patch);
-    plt::add_patch(patch2);
+	plt::add_patch(patch);
+	plt::add_patch(patch2);
+
+	auto axes = plt::gca();
 
 	// Add graph title
-	plt::title("Sample figure");
+	axes.set_title("Sample figure");
+	axes.set_xlabel("X Axis");
+	axes.set_ylabel("Y Axis");
 
-	plt::xlim(0, 20);
-	plt::ylim(0, 20);
+	std::cout << axes.get_title() << std::endl;
+	std::cout << axes.get_xlabel() << std::endl;
+	std::cout << axes.get_ylabel() << std::endl;
 
-    plt::legend();
+	axes.set_xlim(0, 20);
+	axes.set_ylim(0, 20);
+
+	plt::legend();
 
 	// save figure
 	//plt::save("./patches.png");
